c03b04ba92d45294a8a355aff0cadf5b39804ef0
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2025-07-30 11:13:08 +0500

    refactor: add CORS middleware to allow  origins

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from agent.fitness_agent import fitness_agent
 from agents import Runner
+from fastapi.middleware.cors import CORSMiddleware
 from datatypes.datatype import ChatRequest, ChatResponse
 
 
 app = FastAPI()
+
+
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 @app.get("/")
 def home():
